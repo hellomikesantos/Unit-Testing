02de78c8722fbc8e2c77526ae480b5d02a6c1d5c
Michael John Santos <michaeljohnsantos@student.mitt.ca>
2022-10-03 13:14:37 -0500

    Add Check Balance Test Method

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -4,6 +4,7 @@ namespace UnitTest1
     public class UnitTest1
     {
         public decimal StartingBalance { get; set; }
+        public BankAccount TestAccount { get; set; } = new BankAccount();
         public UnitTest1()
         {
             StartingBalance = 500;
@@ -29,6 +30,63 @@ namespace UnitTest1
             // Assert 
             Assert.AreEqual(assertedBalance,
                 account.balance);
+        }
+
+        [TestMethod]
+        public void AccountDeposit_AddsToBalance()
+        {
+            // arrange 
+            decimal depositAmount = 125.32M;
+            decimal currentBalance = 500;
+            decimal assertedBalance = depositAmount + currentBalance;
+
+            BankAccount account = new BankAccount();
+            account.balance = currentBalance;
+
+            // act
+            account.Deposit(depositAmount);
+
+            // assert
+            Assert.AreEqual(assertedBalance,
+                account.balance);
+        }
+
+        //[TestMethod]
+        //public void AccountDeposit_ThrowsExceptionOnZeroOrNegativeAmount()
+        //{
+        //    // arrange
+        //    decimal firstDepositAmount = 0;
+        //    decimal secondDepositAmount = -123;
+
+        //    // act and assert
+        //    Assert.ThrowsException<ArgumentException>(() =>
+        //    {
+        //        TestAccount.Deposit(firstDepositAmount);
+        //    });
+
+        //    Assert.ThrowsException<ArgumentException>(() =>
+        //    {
+        //        TestAccount.Deposit(secondDepositAmount);
+        //    });
+        //}
+
+        [TestMethod]
+        public void AccountBalance_ChecksBalance()
+        {
+            // arrange
+            decimal balance = 500;
+            decimal assertedBalance = balance;
+
+            BankAccount account = new BankAccount();
+            account.balance = balance;
+
+            //act
+            account.CheckBalance();
+
+            // assert
+            Assert.AreEqual(assertedBalance,
+                account.balance);
+            
         }
     }
 }
--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -22,4 +22,29 @@ public class BankAccount
             throw new ArgumentException("Withdraw exceeds available funds.");
         }
     }
+
+    public decimal Deposit(decimal amount)
+    {
+        if(amount >= 0)
+        {
+            balance += amount;
+            return balance;
+        }
+        else
+        {
+            throw new ArgumentException("Amount deposited must be greater than zero");
+        }
+    }
+
+    public decimal CheckBalance()
+    {
+        try
+        {
+            return balance;
+        }
+        catch
+        {
+            throw new ArgumentException("There was an error checking your balance");
+        }
+    }
 }
